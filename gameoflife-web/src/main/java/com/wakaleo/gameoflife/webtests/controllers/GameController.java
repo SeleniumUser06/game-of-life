97f2f3a32cbf3d70c4741fcc0cb59096542a8ed0
SeleniumUser06 <103438884+SeleniumUser06@users.noreply.github.com>
2022-04-16 20:37:33 +0530

    new comment added

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -17,6 +17,7 @@ import com.wakaleo.gameoflife.domain.Universe;
  *
  * GameController.java
  * Controller to navigate the buttons on the game pages
+ * Checking if it works testing
  */
 @Controller
 @RequestMapping("/game")
